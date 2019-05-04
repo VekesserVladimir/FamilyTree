a721133c0779f3177816682779259fbbb37a0ba5
Vladimir <vekesser_vladimir@mail.ru>
2019-05-05 00:59:48 +0400

    Ой! (Внезапный переход на БЭМ)

--- src/FamilyTree.FrontendDraft/Scripts/vuescript.js
+++ src/FamilyTree.FrontendDraft/Scripts/vuescript.js
@@ -1,34 +1,34 @@
 var vm = new Vue({
-	el: '#vue-app',
-	data: {
-		mobileNavigationMenu: {
-			isActive: false,
-			createItemActive: false,
-			browseItemActive: false
-		},
-		searchForm: {
-			isActive: false
-		},
-		personCard: {
-			editingIsActive: false
-		},
-		accountMenu: {
-			isActive: false
-		}
-	}, 
-	methods: {
-		openSearchForm: function() {
-			this.searchForm.isActive = true;
-			document.querySelector('.search-form').style.display = 'inline-block';
-			var form = document.querySelector('.search-field');
-			form.focus();
-			form.select();
-		},
-		closeSearchForm: function() {
-			if(document.querySelector('body').offsetWidth <= 750) {
-				document.querySelector('.search-form').style.display = 'none';
-				this.searchForm.isActive = false;
-			}
-		}
-	}
+    el: '#vue-app',
+    data: {
+        mobileNavigationMenu: {
+            isActive: false,
+            createItemActive: false,
+            browseItemActive: false
+        },
+        searchForm: {
+            isActive: false
+        },
+        personCard: {
+            editingIsActive: false
+        },
+        accountMenu: {
+            isActive: false
+        }
+    },
+    methods: {
+        openSearchForm: function() {
+            this.searchForm.isActive = true;
+            document.querySelector('.search-form').style.display = 'inline-block';
+            var form = document.querySelector('.search-form__field');
+            form.focus();
+            form.select();
+        },
+        closeSearchForm: function() {
+            if (document.querySelector('body').offsetWidth <= 750) {
+                document.querySelector('.search-form').style.display = 'none';
+                this.searchForm.isActive = false;
+            }
+        }
+    }
 })
