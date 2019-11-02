b06f12c015de759d24407a4fc82eb39c932e5bc3
ALotOfNothing <vekesser_vladimir@mail.ru>
2019-11-03 00:00:15 +0400

    Redesign autocomplete

--- src/FamilyTree.FrontendDraft/src/components/SearchForm.vue
+++ src/FamilyTree.FrontendDraft/src/components/SearchForm.vue
@@ -1,18 +1,26 @@
 <template>
 	<form action="" class='search-form header__search-form'>
-		<input type="text" class='search-form__input' v-model="query" v-on:input='search'>
+		<input 
+			type="text" 
+			class='search-form__input'
+			v-model="query" 
+			v-on:input='search'
+			v-bind:class="{ 'search-form_active' : isActive }"
+		>
 		<button class='search-form__button'>
 			<font-awesome-icon icon='search' class='search-form__icon'/>
 		</button>
 		<div class="results" v-if='isActive'>
-			<div class="results__people" v-bind:class='{ results_onlypeople : !getSearchResults.photos.length }'>
+			<div class="results__people">
+				<h4 class="results__block-title">People</h4>
 				<a href='#' class="results__result" v-for='person in getSearchResults.persons' v-bind:key="person.id">
 					<img alt="" class="user-photo results__user-photo" v-bind:src="person.avatarUri">
 					<div class="results__title">{{person.firstName + ' ' + person.lastName}}</div>
 				</a>
 			</div>
 			<div class="line"></div>
-			<div class="results__photos" v-bind:class='{ results_onlyphotos : !getSearchResults.persons.length }'>
+			<div class="results__photos">
+				<h4 class="results__block-title">Photos</h4>
 				<a href='#' class="results__result" v-for='photo in getSearchResults.photos' v-bind:key="photo.id">
 					<img alt="" class="user-photo results__user-photo" v-bind:src='photo.avatarUri'>
 					<div class="results__title">{{photo.title}}</div>
@@ -73,8 +81,14 @@ export default {
 			height: 40px;
 			border-radius: 24px;
 			border: 1px solid #918888;
+
 			padding-left: 20px;
 			font-size: 16px;
+		}
+
+		&_active {
+			border-radius: 24px 24px 0 0;
+			border-bottom: none;
 		}
 
 		&__button {
@@ -95,18 +109,25 @@ export default {
 
 		.results {
 			border: 1px solid #918888;
-			border-radius: 24px;
+			border-top: none;
+			border-radius: 0 0 24px 24px;
 			width: 704px;
 			position: absolute;
-			top: 50px;
+			top: 40px;
 			z-index: 4;
+			background-color: #fff;
+
+			&__block-title {
+				margin: 0 0 10px 20px;
+				color: #1a1a1a;
+				font-weight: 500;
+			}
 
 			&__result {
 				display: flex;
 				align-items: center;
 				text-decoration: none;
-				padding: 5px 10px;
-				background-color: #fff;
+				padding: 10px 20px;
 
 				&:hover {
 					background-color: rgb(180, 233, 209);
@@ -114,27 +135,11 @@ export default {
 			}
 
 			&__people {
-				.results__result:nth-child(1) {
-					border-radius: 24px 24px 0 0;
-				}
+				padding: 20px 0;
 			}
 
 			&__photos {
-				.results__result:nth-last-child(1) {
-					border-radius: 0 0 24px 24px;
-				}
-			}
-
-			&_onlypeople {
-				.results__result:nth-last-child(1) {
-					border-radius: 0 0 24px 24px;
-				}
-			}
-
-			&_onlyphotos {
-				.results__result:nth-child(1) {
-					border-radius: 24px 24px 0 0;
-				}
+				padding: 20px 0;
 			}
 
 			&__user-photo {
